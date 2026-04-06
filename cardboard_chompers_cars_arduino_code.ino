3b3ba561f17ad448f19867ae7f3a9d244cb23993
dmchien000 <77139148+dmchien000@users.noreply.github.com>
2026-04-05 20:58:53 -0500

    Update cardboard_chompers_cars_arduino_code.ino

--- cardboard_chompers_cars_arduino_code.ino
+++ cardboard_chompers_cars_arduino_code.ino
@@ -1,9 +1,37 @@
+// Arduino Cars Enclosure Code
+// Team Cardboard Chompers: David Chien and Srihan Mekala
+
+
+
 void setup() {
-  // Here is the ardiuno code that we'll use to create the Cars narrative enclosure.
+  macServo_180.attach(9);
+  // put your setup code here, to run once:
 
 }
 
 void loop() {
+
+  // Insert code to flip tires over by pitee
+  
+
+
+
+
+
+  macServo_180.write(120);
   // put your main code here, to run repeatedly:
+
+
+
+  // Code for LED lights in Radiator Springs
+
+
+  // Code to have Doc Hudson open door
+
+
+  // Code to have The King light up the finish line when he crosses in the end
+
+
+
 
 }
